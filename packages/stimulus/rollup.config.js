e0416acf208ff693b8025751b17b0ce71e3806de
Sam Stephenson <sam@37signals.com>
2018-10-26 12:09:33 -0500

    WIP: Distribute packages as ES modules

--- packages/stimulus/rollup.config.js
+++ packages/stimulus/rollup.config.js
@@ -1,19 +1,8 @@
 import resolve from "rollup-plugin-node-resolve"
-import { uglify } from "rollup-plugin-uglify"
 import { version } from "../../lerna.json"
 
 const year = new Date().getFullYear()
 const banner = `/*\nStimulus ${version}\nCopyright © ${year} Basecamp, LLC\n */`
-
-const uglifyOptions = {
-  mangle: false,
-  compress: false,
-  output: {
-    beautify: true,
-    indent_level: 2,
-    comments: /Copyright/
-  }
-}
 
 export default {
   input: "index.js",
@@ -24,7 +13,6 @@ export default {
     banner
   },
   plugins: [
-    resolve(),
-    uglify(uglifyOptions)
+    resolve()
   ]
 }
